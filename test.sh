20649238d664bd4d05e9ff5324d131e659a37ae6
Jonathan Zobro <jonathan.zobro@gmail.com>
2021-07-03 12:22:04 -0400

    custom minimal prompt

--- test.sh
+++ test.sh
@@ -4,3 +4,9 @@ shellcheck "init.sh"
 shellcheck "bash/.bash_aliases"
 shellcheck "bash/.bash_profile" -e 1090
 shellcheck "bash/.bashrc" -e 1090,1091,2148,2015 
+shellcheck "bash/.bash/prompt"
+
+#for f in bash/.bash/*
+#do
+#	shellcheck "$f"
+#done
