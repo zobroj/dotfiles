65fdea9a4f8d36ec5d46146faab272607a7efcf8
Jonathan Zobro <jonathan.zobro@gmail.com>
2021-07-04 11:47:16 -0400

    update test

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 shellcheck "init.sh"
 shellcheck "bash/.bash_aliases"
-shellcheck "bash/.bash_profile" -e 1090
+shellcheck "bash/.bash_profile" -e 1090,2148
 shellcheck "bash/.bashrc" -e 1090,1091,2148,2015 
 shellcheck "bash/.bash/prompt"
 
