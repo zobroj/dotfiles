469b3a185d07ec87e5f2345ce9c82186f834c029
Jonathan Zobro <jonathan.zobro@gmail.com>
2021-07-05 15:47:20 -0400

    bash cleanup

--- test.sh
+++ test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-shellcheck "init.sh"
-shellcheck "bash/.bash_aliases"
-shellcheck "bash/.bash_profile" -e 1090,2148
-shellcheck "bash/.bashrc" -e 1090,1091,2148,2015 
-shellcheck "bash/.bash/prompt"
+test_bash () {
+	shellcheck "init.sh"
+	shellcheck "bash/.bash/aliases"
+	shellcheck "bash/.bash/profile" -e 1090,2148
+	shellcheck "bash/.bashrc" -e 1090,1091,2148,2015
+	shellcheck "bash/.bash/prompt"
+}
+
+test_bash
 
 #for f in bash/.bash/*
 #do
